555d120be1980477ae3b1ae97fc6d96f0b8f9aa2
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-11-12 04:05:01 +0000

    dynamic routes wip

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
 require "http"
+require "dotenv/load"
 
 get("/") do
   
@@ -11,4 +12,16 @@ get("/") do
   @all_currencies = @data.fetch("currencies").keys()
 
   erb(:home)
+end
+
+get("/:from_currency") do
+  @root_currency = params.fetch("from_currency")
+
+  all_currencies_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+  response = HTTP.get(all_currencies_url).to_s
+  
+  @all_currencies = JSON.parse(response).fetch("currencies").keys()
+
+  erb(:flexible)
+
 end
