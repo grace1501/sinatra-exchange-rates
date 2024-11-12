52e3d6bed0ba0b4590a6e82134a64a2b05b82812
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-11-12 04:32:00 +0000

    finished. passed all automatic tests.

--- app.rb
+++ app.rb
@@ -19,9 +19,21 @@ get("/:from_currency") do
 
   all_currencies_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
   response = HTTP.get(all_currencies_url).to_s
-  
+
   @all_currencies = JSON.parse(response).fetch("currencies").keys()
 
-  erb(:flexible)
+  erb(:currency_to_convert)
+end
 
+get("/:from_currency/:to_currency") do
+  @root_currency = params.fetch("from_currency")
+  @target_currency = params.fetch("to_currency")
+
+  convert_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}&from=#{@root_currency}&to=#{@target_currency}&amount=1"
+
+  response = HTTP.get(convert_url).to_s
+  response_json = JSON.parse(response)
+  @convert_result = response_json.fetch("result")
+
+  erb(:result)
 end
